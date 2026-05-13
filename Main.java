ad04f3a14233ca69c8c7268b53f312ef56595f04
Adan Bandim <adanlima11@gmail.com>
2026-05-12 22:56:27 -0300

    Carregadores do roteiro

--- Main.java
+++ Main.java
@@ -1,15 +1,42 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        QuestionLoader loader = new QuestionLoader("portuguesequestion.txt");
-    Faceis nivelFacil = new Faceis(loader);
-
-// Para printar apenas a questão 3:
-nivelFacil.printarQuestao(7);
-
-
-
-    }
+    // Carragadores
+    QuestionLoader loader = new QuestionLoader("C:\\Users\\Bandim\\Desktop\\CodeArena_Copy-main\\Questions\\portuguesequestion.txt");
+    ScriptLoader sLoader = new ScriptLoader("C:\\Users\\Bandim\\Desktop\\CodeArena_Copy-main\\CodeArena_Copy-main\\roteiro.txt");
     
+    // Perguntas
+    Faceis nivelFacil = new Faceis(loader);
+    Medias nivelMedio = new Medias(loader);
+    Dificeis nivelDificil = new Dificeis(loader);
+    MuitoDificeis nivelMuitoDificil = new MuitoDificeis(loader);    
+
+    // Fases
+    GameManager game = new GameManager(sLoader);
+    
+    // PRintando roteiro
+    game.exibirIntro();
+    // Escolhendo personagem
+    while (true) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Selecione: 1 Pete 2 Hanny");
+        int escolha = sc.nextInt();
+        if (escolha == 1) {
+            game.escolherPersonagem("PETE");
+            break;
+        } else if (escolha == 2) {
+            game.escolherPersonagem("HANNY");
+            break;
+        } else {
+            System.out.println("Escolhe direito porra");
+            
+        }
+    }
+
+    game.iniciarFase1(true);
+            } 
+        {   
+    } 
 }
